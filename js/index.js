d73812591af9776479921942212e4088afa4cab1
elizabthlee <152847202+elizabthlee@users.noreply.github.com>
2024-06-26 17:37:05 -0700

    add <a> tag to case study

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// $(".logo").hover(function(){
+//     $
+// })
+
+
+// $(document).ready(function(){
+//     $("img").hover(function(){
+//     $(this).attr("src", "Images/logo.png");
+//     }, function(){
+//     $(this).attr("src", "Images/home hover.png");
+//     });
+//     });
